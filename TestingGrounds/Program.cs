9c6288bea2f77b2c34f729e6c9c52bea5537dc96
Andrew Byerly <andrew.Byerly@idcomlog.com>
2018-08-17 12:05:00 -0500

    Sum correctly

--- TestingGrounds/Program.cs
+++ TestingGrounds/Program.cs
@@ -14,10 +14,14 @@ namespace TestingGrounds
             Summer b = new Summer { WeightInPounds = 26 };
 
             List<Summer> c = new List<Summer>();
+            c.Add(a);
+            c.Add(b);
 
             decimal? d = c.Sum(x => x.WeightInPounds);
 
             decimal? e = a.WeightInPounds + b.WeightInPounds;
+
+            bool f = d == 0;
         }
 
         public class Summer
