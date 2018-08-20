d02821562090ebd5cef072aed8d291efeacdbad2
Andrew Byerly <andrew.Byerly@idcomlog.com>
2018-08-20 10:59:18 -0500

    Sum decimals

--- TestingGrounds/Program.cs
+++ TestingGrounds/Program.cs
@@ -13,20 +13,13 @@ namespace TestingGrounds
             Summer a = new Summer { WeightInPounds = null };
             Summer b = new Summer { WeightInPounds = 26 };
 
-            List<Summer> c = new List<Summer>();
-            c.Add(a);
-            c.Add(b);
-
-            decimal? d = c.Sum(x => x.WeightInPounds);
-
-            decimal? e = a.WeightInPounds + b.WeightInPounds;
-
-            bool f = d == 0;
+            decimal? c = a.WeightInPounds + b.WeightInPounds;
         }
 
         public class Summer
         {
             public decimal? WeightInPounds { get; set; }
+           
         }
     }
 }
