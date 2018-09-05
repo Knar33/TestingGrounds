ed7a3088987bc87ea2bd9fbd3b6da11a30da6e9e
Knar33 <adbyerly@comcast.net>
2018-09-04 22:27:17 -0500

    Trying to solve an issue with lazy evaluation of Sum()

--- TestingGrounds/Program.cs
+++ TestingGrounds/Program.cs
@@ -11,7 +11,60 @@ namespace TestingGrounds
     {
         static void Main(string[] args)
         {
-            DateTimeOffset date = DateTimeOffset.Now;
+            List<OrderDetail> orderDetails = new List<OrderDetail>();
+            orderDetails.Add(new OrderDetail { WeightInPounds = null });
+            orderDetails.Add(new OrderDetail { WeightInPounds = null });
+            orderDetails.Add(new OrderDetail { WeightInPounds = null });
+
+            Package package = new Package(orderDetails);
+
+            foreach (PackageDetail packageDetail in package.PackageDetails)
+            {
+                packageDetail.WeightInPounds = 1m;
+            }
+
+            if (package.WeightInPounds != null && package.WeightInPounds != 0)
+            {
+                Console.WriteLine("Not Null or 0");
+            }
+            else
+            {
+                Console.WriteLine("Null or 0");
+            }
+
+            Console.ReadLine();
         }
+    }
+
+    public class Package
+    {
+        public Package(List<OrderDetail> orderDetails)
+        {
+            PackageDetails = new List<PackageDetail>();
+
+            foreach (OrderDetail orderDetail in orderDetails)
+            {
+                PackageDetails.Add(new PackageDetail(orderDetail));
+            }
+
+            WeightInPounds = PackageDetails.Sum(x => x.WeightInPounds);
+        }
+
+        public List<PackageDetail> PackageDetails { get; set; }
+
+        public decimal? WeightInPounds { get; set; }
+    }
+
+    public class PackageDetail
+    {
+        public PackageDetail(OrderDetail orderDetail)
+        {
+        }
+        public decimal? WeightInPounds { get; set; }
+    }
+
+    public class OrderDetail
+    {
+        public decimal? WeightInPounds { get; set; }
     }
 }
