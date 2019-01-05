d2c076899373ac22908ad1be8f1fb5e91c181288
Knar <knarxr@protonmail.com>
2019-01-04 21:41:13 -0600

    wat

--- TestingGrounds/Program.cs
+++ TestingGrounds/Program.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using TestingGrounds.RateService;
+using System.Runtime.CompilerServices;
 
 namespace TestingGrounds
 {
@@ -15,8 +16,12 @@ namespace TestingGrounds
     {
         static void Main(string[] args)
         {
-            Console.Write("Pick 1 for Rock, 2 for Paper, 3 for scissors: ");
-            while (true) Console.WriteLine((new int[] { 1, -2 }).Contains((int.Parse(Console.ReadLine()) - 1) - (new Random()).Next(0, 3)) ? "You Win!" : "You Lose!");
+            ValueTuple<string, string, string> a = Wtfmate("afad");
+        }
+
+        public static (string a, string b, string c) Wtfmate(string d)
+        {
+            return ("wat", "wat", "Wat");
         }
     }
 }
